8a3d66b893dadd450714fed2ceb892da445e061f
Your Name <danieldritter1@gmail.com>
2020-05-18 21:53:14 -0500

    fixing connection script

--- test_connection.py
+++ test_connection.py
@@ -2,6 +2,7 @@ import krpc
 
 
 if __name__ == "__main__":
-    conn = krpc.connect()
+    conn = krpc.connect(name="Example Connection",
+                        address="192.168.1.151", rpc_port=50000, stream_port=50001)
     vessel = conn.space_center.active_vessel
     print(vessel)
